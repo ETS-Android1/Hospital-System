c44fe5a16d2ffb80ae923083ab14b3e822952e8f
MostafaELbadawy <50504490+MostafaELbadawy@users.noreply.github.com>
2020-09-20 04:06:25 +0200

    admin 01

---
+++ app/src/main/java/com/example/hospital/admin.java
@@ -0,0 +1,66 @@
+package com.example.hospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.opengl.EGLExt;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.xml.transform.Result;
+
+public class admin extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+        TabLayout tabLayout=findViewById(R.id.tabLayout);
+        TabItem removeTab=findViewById(R.id.remove_tab);
+        TabItem addTab=findViewById(R.id.add_tab);
+        TabItem editTab=findViewById(R.id.edit_tab);
+        final ViewPager viewPager= findViewById(R.id.viewPager);
+        PagerAdapterFreg pagerAdapter= new PagerAdapterFreg(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+
+
+
+
+
+}
