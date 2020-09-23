016aa05ea3087019c58f2875cb5b2875ccc07f72
MostafaELbadawy <50504490+MostafaELbadawy@users.noreply.github.com>
2020-09-24 01:07:44 +0200

    final admin

--- app/src/main/java/com/example/hospital/DataBase.java
+++ app/src/main/java/com/example/hospital/DataBase.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 
 public class DataBase {
 
-    private static String ip = "192.168.1.54";
+    private static String ip = "192.168.1.3";
     private static String port = "1433";
     private static String Classes = "net.sourceforge.jtds.jdbc.Driver";
     private static String dataBase = "TheHospital";
